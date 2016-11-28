5edb35e351b1e824f19332d1a2c0c7570465e7c6
Wes <wes977@gmail.com>
2016-11-28 03:01:46 -0500

    Now im trying to publich my first service and all that im part way through becuase the damn computer will shut off and all that

--- SOA-pt/SOA-pt/Program.cs
+++ SOA-pt/SOA-pt/Program.cs
@@ -38,10 +38,38 @@ namespace SOA_pt
 
 
             // Publishing the service 
+
+            //publiching the thing and all that
             DRCtemp.teamName = "WestNet";
             DRCtemp.teamID = "1186";
-
             tempWords[0] = tempHL.DRCBuilder(DRCtemp, registryCommands.PUB_SERVICE);
+
+            // All the service information 
+            SRVtemp.teamName = "PT";
+            SRVtemp.serviceName = "PT-SERVICE";
+            SRVtemp.securityLevel = "1";
+            SRVtemp.numARGS = "2";
+            SRVtemp.numResponses = "5";
+            SRVtemp.description = "This totals the purchase and all that jazz";
+            tempWords[1] = tempHL.SRVBuilder(SRVtemp);
+            // ARGS
+            ARGtemp.argPosition = "1";
+            ARGtemp.argName = "";
+            ARGtemp.argDataType = "";
+            ARGtemp.argManOpt = "";
+            tempWords[2] = tempHL.ARGBuilder(ARGtemp);
+
+            ARGtemp.argPosition = "2";
+            ARGtemp.argName = "";
+            ARGtemp.argDataType = "";
+            ARGtemp.argManOpt = "";
+            tempWords[3] = tempHL.ARGBuilder(ARGtemp);
+
+            // RSP
+
+
+            // MCH
+
             tempWords[1] = tempHL.INFBuilder(INFtemp);
             temp.SOAtalking(tempWords);
 
